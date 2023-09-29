d0bf8ac72f1b2caf3c2665d018b104dc709e0121
darienvr <118293353+darienvr@users.noreply.github.com>
2023-09-29 12:55:17 -0500

    add scale in the dark mode button

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -14,7 +14,7 @@ const Home = () => {
     <div className='dark:bg-gray-900 transition dark:text-white min-h-screen'>
         <div className=' mx-auto max-w-[1280px]'>
           <div className='flex justify-end pt-5'>
-            <button onClick={()=>setDarkMode(!darkMode)} className=' text-4xl'>
+            <button onClick={()=>setDarkMode(!darkMode)} className=' text-4xl hover:scale-110 transition-transform'>
               { !darkMode ? <MdDarkMode /> : <BsFillSunFill style={{ color: 'white' }} />}
             </button>
           </div>
