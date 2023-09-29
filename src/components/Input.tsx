182ecf0425bdbc6d8124ef442702e9d3435b4c44
darienvr <118293353+darienvr@users.noreply.github.com>
2023-09-29 12:51:46 -0500

    add hover to the send button

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -12,10 +12,15 @@ const Input = () => {
   };
 
   return (
-    <div className='p-8 mt-28 w-3/6 rounded shadow-lg mx-auto dark:bg-gray-800'>
-        <form action="" className=' max-w-fit flex' onSubmit={handleSubmit}>
-            <input value={input} onChange={(e:React.ChangeEvent<HTMLInputElement>) => setInput(e.target.value)} type="text" className=' bg-slate-100 w-96 rounded border-gray-200 border pl-1' />
-            <button className=' bg-green-400 rounded px-4 py-1 text-white'>Search</button>
+    <div className='p-8 mt-28 w-5/12 rounded shadow-lg mx-auto dark:bg-gray-800'>
+        <form action="" className='flex' onSubmit={handleSubmit}>
+            <input 
+              value={input}
+              onChange={(e:React.ChangeEvent<HTMLInputElement>) => setInput(e.target.value)} 
+              type="text" 
+              className=' bg-slate-100 w-[90%]  rounded-l-md border-gray-200 border pl-1 dark:text-black' 
+            />
+            <button className='bg-green-400 rounded-r-md px-4 py-1 text-white hover:bg-green-500 transition-all hover:font-semibold'>Search</button>
         </form>
     </div>
   )
