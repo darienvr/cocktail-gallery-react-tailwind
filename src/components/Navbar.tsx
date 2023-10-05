8d2b9626d81852de4bea37551b13d4f068feabde
darienvr <118293353+darienvr@users.noreply.github.com>
2023-10-05 12:38:43 -0500

    add dark mode button to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,21 @@
+import { useContext } from 'react'
 import { Link } from "react-router-dom";
+import { AppContext } from '../context'
+import { ContextType } from '../types'
+import { MdDarkMode } from 'react-icons/md'
+import { BsFillSunFill } from 'react-icons/bs'
 
 const Navbar = () => {
+
+  const { darkMode, setDarkMode } = useContext(AppContext) as ContextType
+
   return (
     <>
-      <nav className="bg-gray-50 w-full shadow-md dark:bg-gray-800 dark:text-white"> 
+      <nav className="bg-gray-50 w-full shadow-md dark:bg-gray-800 dark:text-white transition-all"> 
         <div className=" container mx-auto max-w-screen-xl flex items-center justify-between h-24 p-5 ">
             <h1 className=" text-4xl font-bold flex-1">CocktailsDB</h1>
-            <div className="text-right">
-                <ul className="flex gap-8 text-lg">
+            <div className="flex items-center text-right">
+                <ul className="flex gap-8 text-lg mr-20">
                     <li className="hover:scale-110 transition-all">
                       <Link to="/">
                         Home
@@ -19,6 +27,11 @@ const Navbar = () => {
                       </Link>
                     </li>
                 </ul>
+                <div className='flex'>
+                  <button onClick={()=>setDarkMode(!darkMode)} className=' text-4xl hover:scale-110 transition-all'>
+                    { !darkMode ? <MdDarkMode /> : <BsFillSunFill style={{ color: 'white' }} />}
+                  </button>
+                </div>
             </div>
         </div>
       </nav>
