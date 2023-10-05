0b0cdcf3cb47d7493fd36e5e1ca1113fb1cc4ef8
darienvr <118293353+darienvr@users.noreply.github.com>
2023-10-05 12:39:27 -0500

    remove dark mode button

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,23 +1,11 @@
-import { useContext } from 'react'
 import Input from '../components/Input'
 import CocktailsList from '../components/CocktailsList'
-import { MdDarkMode } from 'react-icons/md'
-import { BsFillSunFill } from 'react-icons/bs'
-import { AppContext } from '../context'
-import { ContextType } from '../types'
 
 const Home = () => {
 
-  const { darkMode, setDarkMode } = useContext(AppContext) as ContextType
-
   return (
-    <div className='dark:bg-gray-900 transition dark:text-white min-h-screen'>
-        <div className=' mx-auto max-w-[1280px]'>
-          <div className='flex justify-end pt-5'>
-            <button onClick={()=>setDarkMode(!darkMode)} className=' text-4xl hover:scale-110 transition-transform'>
-              { !darkMode ? <MdDarkMode /> : <BsFillSunFill style={{ color: 'white' }} />}
-            </button>
-          </div>
+    <div className='dark:bg-gray-900 dark:text-white min-h-screen transition-all'>
+        <div className=' mx-auto max-w-[1280px] pt-20'>
           <Input />
           <CocktailsList />
       </div>
